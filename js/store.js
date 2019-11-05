8ab3f3cb99f42e41150c0f831972b0329d6dff76
arif_shaikh <arif.shaikh@netcentric.biz>
2019-11-05 15:28:14 +0530

    Store added

---
+++ js/store.js
@@ -0,0 +1,10 @@
+const games_catlog = [{
+    "name": "Counter Strike",
+    "cpu": "Intel Core i5-2500K",
+    "cpu_speed": "1.7 GHZ",
+    "OS" : "Windows 7 (32/64-bit)/Vista/XP",
+    "ram" : "512 MB",
+    "video_card": "DirectX 8.1 level Graphics Card",
+    "sound_card":"Required",
+    "disk_space": "4.6GB"
+}];
